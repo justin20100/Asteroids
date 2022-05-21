b16e3ebe6ebf45329f0fe2cfba40ac39ff17e07e
justinvincent <justin.vincent@student.hepl.be>
2022-05-21 15:41:03 +0200

    end video 14

---
+++ src/controller.js
@@ -0,0 +1,25 @@
+const controller = {
+    keys:{
+        'ArrowUp': - 1,
+    },
+    activeKeys:[],
+
+    init(){
+        document.addEventListener('keydown',(e)=>{
+            e.preventDefault();
+            e.stopPropagation();
+            if (Object.keys(this.keys).includes(e.key) && !this.activeKeys.includes(e.key)){
+                this.activeKeys.push(e.key);
+            }
+        })
+        document.addEventListener('keyup',(e)=>{
+            e.preventDefault();
+            e.stopPropagation();
+            if (this.activeKeys.includes(e.key)){
+                const i = this.activeKeys.indexOf(e.key)
+                this.activeKeys.splice(i,1);
+            }
+        })
+    }
+}
+export default controller
--- src/ship.js
+++ src/ship.js
@@ -1,21 +1,32 @@
+import controller from "./controller";
+
 const ship = {
 
   size: 20,
-  speedX: 1,
-  locationX: 0,
+  speedY: 0,
+  locationY: 0,
   canvas: null,
   ctx: null,
 
   init (canvas, ctx) {
+    controller.init();
     this.canvas = canvas
     this.ctx = ctx
-    this.locationX = this.canvas.width / 2
+    this.locationY = this.canvas.height / 2
   },
 
   update () {
-    this.locationX += this.speedX
-    if (this.locationX > this.canvas.width + this.size) {
-      this.locationX = -this.size
+    controller.activeKeys.forEach((activeKey)=>{
+      this.speedY += controller.keys[activeKey] * 0.05;
+    })
+
+    this.locationY += this.speedY
+    if (this.locationY > this.canvas.height + this.size) {
+      this.locationY = -this.size
+    }
+    this.locationY += this.speedY
+    if (this.locationY < -this.size) {
+      this.locationY = this.canvas.height + this.size
     }
     this.draw()
   },
@@ -23,7 +34,7 @@ const ship = {
   draw () {
     this.ctx.save()
     this.ctx.rotate(0)
-    this.ctx.translate(this.locationX, this.canvas.height / 2)
+    this.ctx.translate( this.canvas.width / 2, this.locationY,)
     this.ctx.beginPath()
     this.ctx.moveTo(0, -1.5 * this.size / 2)
     this.ctx.lineTo(this.size / 2, 0.5 + (this.size * 1.5 / 2))
