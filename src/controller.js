7868dc42085601062ed3970f55567b6877d66294
justinvincent <justin.vincent@student.hepl.be>
2022-06-03 12:25:10 +0200

    end video 19

--- src/controller.js
+++ src/controller.js
@@ -1,25 +1,27 @@
 const controller = {
-    keys:{
-        'ArrowUp': - 1,
+    keys: {
+        'ArrowUp': -1,
+        'ArrowRight': Math.PI / 15,
+        'ArrowLeft': -Math.PI / 15,
+        ' ': 1,
     },
-    activeKeys:[],
-
-    init(){
-        document.addEventListener('keydown',(e)=>{
-            e.preventDefault();
-            e.stopPropagation();
-            if (Object.keys(this.keys).includes(e.key) && !this.activeKeys.includes(e.key)){
+    activeKeys: [],
+    init() {
+        document.addEventListener('keydown', (e) => {
+            if (Object.keys(this.keys).includes(e.key) && !this.activeKeys.includes(e.key)) {
+                e.preventDefault();
+                e.stopPropagation();
                 this.activeKeys.push(e.key);
             }
         })
-        document.addEventListener('keyup',(e)=>{
-            e.preventDefault();
-            e.stopPropagation();
-            if (this.activeKeys.includes(e.key)){
+        document.addEventListener('keyup', (e) => {
+            if (this.activeKeys.includes(e.key)) {
+                e.preventDefault();
+                e.stopPropagation();
                 const i = this.activeKeys.indexOf(e.key)
-                this.activeKeys.splice(i,1);
+                this.activeKeys.splice(i, 1);
             }
         })
     }
 }
-export default controller
+export default controller;
--- src/ship.js
+++ src/ship.js
@@ -1,40 +1,71 @@
 import controller from "./controller";
+import Vector from "./Vector";
 
 const ship = {
 
   size: 20,
-  speedY: 0,
-  locationY: 0,
+  speed: null,
+  heading: 0,
+  location: null,
+  acceleration: null,
   canvas: null,
   ctx: null,
+  bulletTimer: -1,
+  bulletTimerTreshold: 10,
 
   init (canvas, ctx) {
     controller.init();
     this.canvas = canvas
     this.ctx = ctx
-    this.locationY = this.canvas.height / 2
+    this.location = new Vector(this.canvas.width / 2,this.canvas.height / 2)
+    this.speed = new Vector(0,0)
   },
 
   update () {
     controller.activeKeys.forEach((activeKey)=>{
-      this.speedY += controller.keys[activeKey] * 0.05;
-    })
+      if (activeKey === 'ArrowUp'){
+        this.acceleration = Vector.fromAngle(this.heading);
+        this.speed.add(this.acceleration);
 
-    this.locationY += this.speedY
-    if (this.locationY > this.canvas.height + this.size) {
-      this.locationY = -this.size
+      }else if(activeKey === 'ArrowRight' || activeKey === 'ArrowLeft'){
+        this.updateHeading(controller.keys[activeKey])
+
+      }else if (activeKey === ' '){
+        this.bulletTimer++
+        if (!(this.bulletTimer%this.bulletTimerTreshold)){
+          console.log('ca tire!');
+        }else {
+          this.bulletTimer = 0;
+        }
+      }
+    })
+    this.speed.multiply(0.95);
+    this.location.add(this.speed);
+
+    // passer de bord en bord
+    if (this.location.y > this.canvas.height + this.size) {
+      this.location.y = -this.size
     }
-    this.locationY += this.speedY
-    if (this.locationY < -this.size) {
-      this.locationY = this.canvas.height + this.size
+    if (this.location.y < -this.size) {
+      this.location.y = this.canvas.height + this.size
+    }
+    if (this.location.x > this.canvas.width + this.size) {
+      this.location.x = -this.size
+    }
+    if (this.location.x < -this.size) {
+      this.location.x = this.canvas.width + this.size
     }
     this.draw()
   },
 
+  updateHeading(angle){
+    this.heading += angle;
+  },
+
   draw () {
     this.ctx.save()
-    this.ctx.rotate(0)
-    this.ctx.translate( this.canvas.width / 2, this.locationY,)
+    this.ctx.translate( this.location.x , this.location.y);
+    this.ctx.rotate(this.heading);
     this.ctx.beginPath()
     this.ctx.moveTo(0, -1.5 * this.size / 2)
     this.ctx.lineTo(this.size / 2, 0.5 + (this.size * 1.5 / 2))
@@ -43,7 +74,6 @@ const ship = {
     this.ctx.stroke()
     this.ctx.restore()
   }
-
 }
 
 export default ship
